6d2bd9212c06b71eba27b736fd372adc6f5bcb64
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-06-21 17:53:10 +0500

    Agreement and start messages created

---
+++ messages/agreement.js
@@ -0,0 +1,26 @@
+const total = (product) => Number(product.quantity) * Number(product.price);
+
+function getProduct(product, lang) {
+  return `
+${product[`title_${lang}`]}: 
+    ${product.quantity} X ${product.price} = ${total(product)}
+`;
+}
+
+function getTotal(products) {
+  return products.map((product) => total(product)).reduce((a, b) => a + b, 0);
+}
+
+export default (ctx, fullName, phone, dType, products) => `
+${ctx.t('agreementFullName')}: ${fullName}
+${ctx.t('agreementPhone')}: ${phone}
+${ctx.t('agreementType')}: ${dType}
+~~~~~~~~~~~~~~~~~~~
+${
+  products.map((product) => getProduct(product, ctx.session.lang)).join('')
+}
+~~~~~~~~~~~~~~~~~~~
+${ctx.t('agreementTotal')}: ${getTotal(products)}
+${ctx.t('agreementDeliveryPrice')}: ${ctx.t('agreementDeliveryMessage')}
+${ctx.t('agreementTotalWithout')}: ${getTotal(products)}
+`;
