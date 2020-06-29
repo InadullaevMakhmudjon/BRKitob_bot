6318761eb7a1c01a31c70e5b8b91b3718d7b9c06
unknown <mr.azik98@mail.ru>
2020-06-29 22:53:59 +0500

    added myProfile button to main menu

--- keyboards/categories.js
+++ keyboards/categories.js
@@ -2,7 +2,7 @@ import Markup from 'telegraf/markup';
 
 export const gifts = (text) => Markup.button(text);
 export const books = (text) => Markup.button(text);
-export const bonuses = (text) => Markup.button(text);
+export const profile = (text) => Markup.button(text);
 
 // eslint-disable-next-line max-len
-export default ({ gifts: g, books: book, bonuses: bonus }) => [gifts(g), books(book), bonuses(bonus)];
+export default ({ gifts: g, books: book, profile: p }) => [gifts(g), books(book), profile(p)];
