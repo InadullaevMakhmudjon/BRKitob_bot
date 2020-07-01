31928d5a613c66caa04aaed815da8abf9e5f3fa4
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-07-02 01:06:59 +0500

    Loading animation added to payments

--- actions/payme.js
+++ actions/payme.js
@@ -1,5 +1,6 @@
 import Orders from '../service/orders';
 import main from '../hears/main';
+import { LOADING } from '../utils/stickers';
 
 const createOrder = ({ order, user, shopping }) => new Promise((resolve) => {
   Orders.create({
@@ -10,6 +11,7 @@ const createOrder = ({ order, user, shopping }) => new Promise((resolve) => {
 });
 
 export default async (ctx, next) => {
+  const { message_id, chat: { id: chatId } } = await ctx.replyWithSticker(LOADING);
   await ctx.answerCbQuery();
   if (!ctx.session.order) {
     ctx.session.order = {};
@@ -17,6 +19,7 @@ export default async (ctx, next) => {
   ctx.session.order.method = 2;
   await createOrder(ctx.session);
   const { url } = await createOrder(ctx.session);
-  ctx.reply(url);
+  await ctx.reply(url);
+  await ctx.telegram.deleteMessage(chatId, message_id);
   main(ctx, next);
 };
--- actions/telegram.js
+++ actions/telegram.js
@@ -1,6 +1,7 @@
 import invoice from '../hears/invoice';
 import Orders from '../service/orders';
 import main from '../hears/main';
+import { LOADING } from '../utils/stickers';
 
 const createOrder = ({ order, user, shopping }) => new Promise((resolve) => {
   Orders.create({
@@ -11,6 +12,7 @@ const createOrder = ({ order, user, shopping }) => new Promise((resolve) => {
 });
 
 export default async (ctx, next) => {
+  const { message_id, chat: { id: chatId } } = await ctx.replyWithSticker(LOADING);
   await ctx.answerCbQuery();
   if (!ctx.session.order) {
     ctx.session.order = {};
@@ -18,5 +20,6 @@ export default async (ctx, next) => {
   ctx.session.order.method = 1;
   await createOrder(ctx.session);
   await invoice(ctx, ctx.session.shopping);
+  await ctx.telegram.deleteMessage(chatId, message_id);
   main(ctx, next);
 };
